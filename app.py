9240a44352d475abbf60ca0c053235083cdf7229
KyleFromAllCloud <96033056+KyleFromAllCloud@users.noreply.github.com>
2024-01-30 09:02:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,29 +18,29 @@ if st.button("Run AWS CLI Command"):
     if not user_input:
         st.error("Please enter an Intent Name.")
     else:
-        try:
+        # try:
             # Create an S3 client using boto3 with the loaded credentials
-            session = boto3.client(
-                'lexv2-models',
-                aws_access_key_id=aws_access_key_id,
-                aws_secret_access_key=aws_secret_access_key,
-                region_name=aws_region
+        session = boto3.client(
+            'lexv2-models',
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key,
+            region_name=aws_region
+        )
+
+        # List objects in the specified S3 bucket
+        objects = session.list_intents(
+            botId='7LIOJYDHIB',
+            botVersion='DRAFT',
+            localeId='en_US'
             )
 
-            # List objects in the specified S3 bucket
-            objects = session.list_intents(
-                botId='7LIOJYDHIB',
-                botVersion='DRAFT',
-                localeId='en_US'
-                )
-
-            # Display the result
-            st.subheader("Objects in S3 Bucket:")
-            if "Contents" in objects:
-                for obj in objects["Contents"]:
-                    st.write(obj["Key"])
-            else:
-                st.write("No objects found in the specified bucket.")
+        # Display the result
+        st.subheader("Objects in S3 Bucket:")
+        if "Contents" in objects:
+            for obj in objects["Contents"]:
+                st.write(obj["Key"])
+            # else:
+            #     st.write("No objects found in the specified bucket.")
 
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
